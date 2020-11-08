e2d443212b1c67af3e22de402ea64aef1e7fb1df
ram <ramanujan.jhmi@gmail.com>
2020-11-08 15:29:19 -0500

    all data. added data splitting in the collate script. some documentation.

--- ep/ep_data_collate.m
+++ ep/ep_data_collate.m
@@ -1,24 +1,33 @@
+% This script is for internal purposes only.
+% It lets me collate data from various sources and create a unified data
+% file that contains the shape specifications and neural responses for each
+% unit in V4 across monkeys. This file can then be used to generate all the
+% stimuli and reproduce all analyses.
+
 clear; clc;
 
 projPath = '/Volumes/Supportboy/cold/Hopkins/NHP/prod/EP/projectMaskedGA3D';
 load([projPath '/analysis/plots/population/ids.mat'],'population');
+splits = 1:10:length(population);
 
 monkeyNames = {'dobby' 'merri' 'gizmo'};
-sCount = 1;
-for ii=60:60 % 1:length(population)
-    nGen = population(ii).nGen-population(ii).nPostHoc;
-    for gg=1:nGen
-        load([projPath '/stim/' monkeyNames{population(ii).monkeyId} '/' num2str(population(ii).prefix) '_r-' num2str(population(ii).runNum) '_g-' num2str(gg) '/stimParams.mat'])
-        stimuli = stimuli'; stimuli = stimuli(:);
-        for ss=1:length(stimuli)
-            shapeResp(sCount).id = stimuli{ss}.id.descId;
-            shapeResp(sCount).col = stimuli{ss}.shape.color;
-            shapeResp(sCount).tex = stimuli{ss}.shape.texture;
-            shapeResp(sCount).spec = stimuli{ss}.shape.mstickspec;
-            shapeResp(sCount).resp = mean(stimuli{ss}.id.respMatrix);
-            sCount = sCount+1;
+for jj=1:length(splits)-1
+    sCount = 1;
+    for ii=splits(jj):splits(jj+1)
+        nGen = population(ii).nGen-population(ii).nPostHoc;
+        for gg=1:nGen
+            load([projPath '/stim/' monkeyNames{population(ii).monkeyId} '/' num2str(population(ii).prefix) '_r-' num2str(population(ii).runNum) '_g-' num2str(gg) '/stimParams.mat'])
+            stimuli = stimuli'; stimuli = stimuli(:);
+            for ss=1:length(stimuli)
+                shapeResp(sCount).id = stimuli{ss}.id.descId;
+                shapeResp(sCount).col = stimuli{ss}.shape.color;
+                shapeResp(sCount).tex = stimuli{ss}.shape.texture;
+                shapeResp(sCount).spec = stimuli{ss}.shape.mstickspec;
+                shapeResp(sCount).resp = mean(stimuli{ss}.id.respMatrix);
+                sCount = sCount+1;
+            end
         end
     end
-end
 
-save('export/phys_shapeResp.mat','shapeResp')
+    save(['export/phys_shapeResp_' num2str(jj) '.mat'],'shapeResp')
+end
--- ep/ep_data_img.m
+++ ep/ep_data_img.m
@@ -1,5 +1,11 @@
+% This script generates the image for any stimulus - 3d or 2d
+% It loads the data file, gets the details of the stimulus, and calls the
+% java executable that generates the image. The spec and image are
+% temporarily stored in the export folder.
+% Note: The java executables have been tested for JDK 8 and below.
+
 clear; clc;
-load('export/phys_shapeResp.mat','shapeResp')
+load('export/phys_shapeResp_4.mat','shapeResp')
 
 imgId = 1;
 fore_color = ceil(shapeResp(imgId).col);
